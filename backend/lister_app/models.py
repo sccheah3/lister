2205399bbe78ee43f77fba5254f25f75bee62c1b
Stanley Cheah <sccheah3@gmail.com>
2020-01-29 23:06:15 -0800

    create list model

--- backend/lister_app/models.py
+++ backend/lister_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+	parent_list = models.ForeignKey('self', related_name='tasks', null=True, on_delete=models.CASCADE)
+	title = models.CharField(max_length=256)
+	detail = models.TextField()
+	is_complete = models.BooleanField(default=False)
+
+	modified_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
---
+++ backend/lister_app/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from lister_app.models import List
+
+
+class ListModelTest(TestCase):
+
+	def test_saving_and_retrieving_single_level_list(self):
+		list_1 = List.objects.create(title="Go to work.",
+									 detail="Need to get to work in San Jose every morning by 9am.")
+
+		list_2 = List.objects.create(title="Go to the gym",
+									 detail="Goal is to gain to 175 lbs.")
+
+		saved_lists = List.objects.all()
+
+		self.assertEqual(list_1.title, saved_lists[0].title)
+		self.assertEqual(list_1.detail, saved_lists[0].detail)
+
+		self.assertEqual(list_2.title, saved_lists[1].title)
+		self.assertEqual(list_2.detail, saved_lists[1].detail)
+
+
+	def test_multi_level_list(self):
+		list_1 = List.objects.create(title="Go to work.",
+									 detail="Need to get to work in San Jose every morning by 9am.")
+
+		list_2 = List.objects.create(parent_list=list_1,
+									 title="Go to the gym",
+									 detail="Goal is to gain to 175 lbs.")
+
+		task = List.objects.all()[0].tasks.all()[0]
+
+		self.assertEqual(task, list_2)
+		self.assertEqual(task.title, list_2.title)
+		self.assertEqual(task.detail, list_2.detail)
