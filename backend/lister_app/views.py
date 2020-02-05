04a78cbfdf21dee6a15e4f08a97073fad83955d9
Stanley Cheah <sccheah3@gmail.com>
2020-02-05 14:47:48 -0800

    able to filter for list with no parent

--- backend/lister_app/views.py
+++ backend/lister_app/views.py
@@ -4,11 +4,20 @@ from lister_app.serializers import ListSerializer
 from lister_app.models import List
 
 from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
 
+
+class ListFilter(django_filters.FilterSet):
+	is_root = django_filters.BooleanFilter(field_name="parent_list", lookup_expr="isnull")
+
+	class Meta:
+		model = List
+		fields = ['parent_list', 'is_root', 'title', 'detail']
 
 class ListViewSet(viewsets.ModelViewSet):
 	serializer_class = ListSerializer
 	queryset = List.objects.all()
 
 	# filter_backends = [DjangoFilterBackend]
-	filterset_fields = ['parent_list']
+	#filterset_fields = ['parent_list', 'title']
+	filterset_class = ListFilter
