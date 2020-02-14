f6d79b3178f499647275cbf08d81dc7a7a35424f
Stanley Cheah <sccheah3@gmail.com>
2020-02-13 23:41:18 -0800

    include serialize email field during override create method

--- backend/accounts_app/serializers.py
+++ backend/accounts_app/serializers.py
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
     def create(self, validated_data):
-        user = User.objects.create(username=validated_data['username'])
+        user = User.objects.create(username=validated_data['username'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
 
