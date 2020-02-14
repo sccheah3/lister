e81ea5112c373d85d4db30bcc482e667bd41fc14
Stanley Cheah <sccheah3@gmail.com>
2020-02-13 23:42:04 -0800

    override create. does same thing, prep for auth

--- backend/accounts_app/views.py
+++ backend/accounts_app/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
 from accounts_app.serializers import UserSerializer
 from django.contrib.auth import get_user_model
 
@@ -13,3 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.AllowAny
     ]
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        })
