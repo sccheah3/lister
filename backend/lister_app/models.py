55cf2597eb4d5f169b0ab27bcd7a147ab0187dbf
Stanley Cheah <sccheah3@gmail.com>
2020-04-29 23:49:07 -0700

    remove details field from mode

--- backend/lister_app/models.py
+++ backend/lister_app/models.py
@@ -7,7 +7,6 @@ class List(models.Model):
 	parent_list = models.ForeignKey('self', related_name='tasks', null=True, on_delete=models.CASCADE)
 	owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='lists', null=True, on_delete=models.CASCADE)
 	title = models.CharField(max_length=256)
-	detail = models.TextField(blank=True)
 	is_complete = models.BooleanField(default=False)
 
 	modified_at = models.DateTimeField(auto_now=True)
